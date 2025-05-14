53913003460f5bdd05476c4f6417559e5e0bd914
Rifqi <rifqiazz69@gmail.com>
2025-05-14 23:31:20 +0700

    update interaktif web

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,40 @@
 //navbar fixed
-window.onscroll= function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
 
-    if (window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    };
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    header.classList.remove("navbar-transparent");
+  } else {
+    header.classList.remove("navbar-fixed");
+    header.classList.add("navbar-transparent");
+  }
 };
 
 //Hamburger
 
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Smooth Scrolling
+const navLinks = document.querySelectorAll('a[href^="#"]');
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
 });
